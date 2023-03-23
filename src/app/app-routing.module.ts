661369d25ddf1b390846087d4c817175591efb14
IrvinFrias <irvin.frias.unam@outlook.com>
2023-03-23 17:49:08 -0600

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {UsersPanelComponent} from "./users-panel/users-panel.component";
+import {UsersFormComponent} from "./users-form/users-form.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'users', component: UsersPanelComponent},
+  {path:'form', component: UsersFormComponent},
+  {path: '**', redirectTo: 'users' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersPanelComponent } from './users-panel/users-panel.component';
+import { UsersFormComponent } from './users-form/users-form.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersPanelComponent,
+    UsersFormComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
